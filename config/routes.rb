f4f41fe40f5f1271a82e738666fa17a89760d0e1
learncbrainy-loop8787 <koreykendrick1@gmail.com>
2021-02-25 09:04:17 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :unit_warriors, only: [:index, :create, :destroy, :show]
+  resources :units, only: [:index, :show]
+  resources :warriors, only: [:index, :show]
+  resources :users, only: [:create, :show]
+
+  post '/dday' => 'units#dday'
+
+  get '/sort_kills_asc' => 'warriors#sort_kills_asc'
+  get '/sort_kills_desc'=> 'warriors#sort_kills_desc'
+
+  get '/find_single' => 'warriors#find_single'
+  get '/find_thousands' => 'warriors#find_thousands'
+  get '/find_millions' => 'warriors#find_millions'
+ 
+
+  get '/find_master' => 'warriors#find_master'
+  get '/find_intermediate' => 'warriors#find_legend'
+  get '/find_god' => 'warriors#find_god'
+
+
+
+
 end
